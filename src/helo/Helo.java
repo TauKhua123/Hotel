161a66c27c50ad6d726b5ce99e1f4d0916590455
ta2khu75 <ta2khu75@ta2khu75-IdeaPad-Gaming-3-15ACH6>
2023-11-08 10:36:46 +0700

    commit project

---
+++ src/helo/Helo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helo;
+
+/**
+ *
+ * @author ta2khu75
+ */
+public class Helo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("fdsafdafdaaf");
+    }
+    
+}
